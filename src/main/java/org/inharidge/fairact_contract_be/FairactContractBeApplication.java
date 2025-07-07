c22ec83063298429bf688ff24fadfa8383a2c24c
HEX <123macanic@naver.com>
2025-07-08 02:33:17 +0900

    feat: Spring 프로젝트 환경설정 추가

---
+++ src/main/java/org/inharidge/fairact_contract_be/FairactContractBeApplication.java
@@ -0,0 +1,13 @@
+package org.inharidge.fairact_contract_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairactContractBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FairactContractBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/inharidge/fairact_contract_be/FairactContractBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inharidge.fairact_contract_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairactContractBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
