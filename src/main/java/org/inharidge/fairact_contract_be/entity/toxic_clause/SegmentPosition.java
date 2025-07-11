7f632e7e2ee7112aa0ee3c546e5048fa19cb2d13
HEX <123macanic@naver.com>
2025-07-12 01:31:22 +0900

    feat: Contract 스키마 구조 변경에 따른 추가 클래스

---
+++ src/main/java/org/inharidge/fairact_contract_be/entity/toxic_clause/SegmentPosition.java
@@ -0,0 +1,15 @@
+package org.inharidge.fairact_contract_be.entity.toxic_clause;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SegmentPosition {
+    private int pageIndex;
+    private List<Vertex> vertices;
+}
