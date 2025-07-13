af49c1bf688980603eaf0e593950bd3c6718f7fa
HEX <123macanic@naver.com>
2025-07-13 18:31:17 +0900

    fix: ToxicClause Auto Generate Id column Prob Solved

--- src/main/java/org/inharidge/fairact_contract_be/service/ToxicClauseService.java
+++ src/main/java/org/inharidge/fairact_contract_be/service/ToxicClauseService.java
@@ -1,37 +1,33 @@
 package org.inharidge.fairact_contract_be.service;
 
+import org.inharidge.fairact_contract_be.entity.Contract;
 import org.inharidge.fairact_contract_be.entity.toxic_clause.ToxicClause;
 import org.inharidge.fairact_contract_be.exception.NotFoundContractException;
 import org.inharidge.fairact_contract_be.repository.ContractRepository;
-import org.inharidge.fairact_contract_be.repository.ToxicClauseRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.util.List;
 
 @Service
 public class ToxicClauseService {
-    private final ToxicClauseRepository toxicClauseRepository;
     private final ContractRepository contractRepository;
 
-    public ToxicClauseService(ToxicClauseRepository toxicClauseRepository, ContractRepository contractRepository) {
-        this.toxicClauseRepository = toxicClauseRepository;
+    public ToxicClauseService(ContractRepository contractRepository) {
         this.contractRepository = contractRepository;
     }
 
     public void updateToxicClausesCheckState(String contractId) {
-        List<ToxicClause> toxicClauses =
+        Contract contract =
                 contractRepository.findById(contractId)
-                        .orElseThrow(() -> new NotFoundContractException("contractId : " + contractId))
-                        .getClauses();
+                        .orElseThrow(() -> new NotFoundContractException("contractId : " + contractId));
 
-        if(toxicClauses != null) {
-            for (ToxicClause toxicClause : toxicClauses) {
+        if(contract.getClauses() != null) {
+            for (ToxicClause toxicClause : contract.getClauses()) {
                 toxicClause.setIsChecked(true);
                 toxicClause.setCheckedAt(Instant.now().getEpochSecond());
             }
 
-            toxicClauseRepository.saveAll(toxicClauses);
+            contractRepository.save(contract);
         }
     }
 }
