6c8c471ccd7b2a490bdc854958b29618ad274544
HEX <123macanic@naver.com>
2025-07-15 15:26:45 +0900

    fix: from value setting

--- src/main/java/org/inharidge/fairact_contract_be/service/EmailService.java
+++ src/main/java/org/inharidge/fairact_contract_be/service/EmailService.java
@@ -3,6 +3,7 @@ package org.inharidge.fairact_contract_be.service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -13,10 +14,14 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String from;
+
     public void sendHtmlEmail(String to, String subject, String htmlContent) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
 
         MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
+        helper.setFrom(from);
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(htmlContent, true); // true → HTML 형식으로 보냄
